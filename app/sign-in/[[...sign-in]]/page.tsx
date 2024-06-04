37e788a6487ffd590e97e4e64d00eab5296732fe
bananasplit333 <j.aehyon.yoo@gmail.com>
2024-06-03 23:25:19 -0400

    updated signin page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-slate-200">
+        <SignIn routing="path" path="/sign-in" />
+    </div>
+  );
 }
--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -6,7 +6,6 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from './Typography';
 import { SignIn, useClerk } from '@clerk/nextjs';
-import Link from 'next/link';
 
 export default function CallToAction() {
     const openSignIn = () => {
@@ -34,13 +33,13 @@ export default function CallToAction() {
                 <Typography variant="h4" component="h2" sx={{ mb: 4 }}>
                     Start streamlining your expense tracking today!
                 </Typography>
-                <Link href="/dashboard" className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-white rounded-full shadow-md group" onClick={() => openSignIn()}>
+                <a href="/dashboard" className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-white transition duration-300 ease-out border-2 border-white rounded-full shadow-md group" onClick={() => openSignIn()}>
                     <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
                     </span>
                     <span className="absolute flex items-center justify-center w-full h-full text-white transition-all duration-300 transform group-hover:translate-x-full ease">Upload Your First Receipt</span>
                     <span className="relative invisible">Upload Your First Receipt</span>
-                </Link>
+                </a>
             </Container>
         </Box>
     );
